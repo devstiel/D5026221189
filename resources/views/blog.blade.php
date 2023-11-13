49798d3b4db5c324e1a5d06e82754910a0ffed84
Devy Relliani <devy.reliani.s@gmail.com>
2023-11-13 15:34:58 +0700

    Pertemuan 13 Nov

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -6,7 +6,7 @@
 
     <body>
         <h3> www.malasngoding.com </h3>
-        <p>Seri Tutorial Laravel Lengkap Dari Dasar</p>
+        <p>Seri Tutorial Laravel Lengkap Dari Dasar!</p>
         <p>Ini adalah view blog. ada di note Blog</p>
         <p>
             Nama    : {{$nama}} <br>
