f599e628049ba768459778444f51660856a06e3b
Devy Relliani <devy.reliani.s@gmail.com>
2023-12-03 19:47:20 +0700

    Tugas Pertemuan Tanggal 27 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -16,12 +16,6 @@
 	<form action="/pegawai/update" method="post">
 	<form action="/pegawai/update" method="post" class="form-horizontal">
 		{{ csrf_field() }}
-		<input type="hidden" name="id" value="{{ $p->pegawai_id }}"> <br/>
-		Nama <input type="text" required="required" name="nama" value="{{ $p->pegawai_nama }}"> <br/>
-		Jabatan <input type="text" required="required" name="jabatan" value="{{ $p->pegawai_jabatan }}"> <br/>
-		Umur <input type="number" required="required" name="umur" value="{{ $p->pegawai_umur }}"> <br/>
-		Alamat <textarea required="required" name="alamat">{{ $p->pegawai_alamat }}</textarea> <br/>
-		<input type="submit" value="Simpan Data">
         <div class = "form-group">
             <label for = "firstname" class = "col-sm-2 control-label">Nama</label>
             <div class = "col-sm-8">
